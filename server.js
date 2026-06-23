f0809b75c7ed80dad5aa9f54d78856914795d8a9
pjimena <pjimena@M0016>
2026-06-23 17:10:46 +0800

    Fix Vercel deployment config

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log('Server is starting...');
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Running on port ${PORT}`));
+
+module.exports = app;
